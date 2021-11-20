ce244164c802d5bb22a4b309b289a7c795b06b16
GiftMonwa <84226371+GiftMonwa@users.noreply.github.com>
2021-11-21 01:43:35 +0200

    Add files via upload

--- System/VacuumEngine.cpp
+++ System/VacuumEngine.cpp
@@ -7,42 +7,50 @@ VacuumEngine::VacuumEngine(int num1,int num2,int num3,int num4):Engine(num1,num2
 
 bool VacuumEngine::testEngine()
 {
-    EngineStore* storage=new EngineStore();
-    storage->storeMemento(this->createMemento());    //store the Initial Temp and fuel of engine before testing
-    fuel-=28; 
-    temp+=8;
-    if(fGradient>=30 && fGradient<60)  //Flow gradi3ent between 30 and 40
+    if(on)
     {
-        status=true;
-        if(pressure>=50 && pressure<100 && status)  //oil pressure  between 30 and 100 
+        EngineStore* storage=new EngineStore();
+        storage->storeMemento(this->createMemento());    //store the Initial Temp and fuel of engine before testing
+        fuel-=28; 
+        temp+=8;
+        if(fGradient>=30 && fGradient<60)  //Flow gradi3ent between 30 and 40
         {
-            fuel-=28;
-            temp+=10;
-            if(temp>=200 && temp<350 && status)  //Temperature between 200 and 350
+            status=true;
+            if(pressure>=50 && pressure<100 && status)  //oil pressure  between 30 and 100 
             {
                 fuel-=28;
+                temp+=10;
+                if(temp>=200 && temp<350 && status)  //Temperature between 200 and 350
+                {
+
+                    retrieveMemento(storage->retrieve());     //restore the original fuel and temperature values 
+                }
+                else
+                {
+                    cout<<"Vacumm Engine  had abnormal Temperature"<<endl;
+                    status=false;
+                }
+
             }
             else
             {
-                cout<<"Vacumm Engine  had abnormal Temperature"<<endl;
+                cout<<"Vacuum Engine  had abnormal Oil pressure"<<endl;
                 status=false;
             }
-
         }
         else
         {
-            cout<<"Vacuum Engine  had abnormal Oil pressure"<<endl;
+            cout<<"Vacuum Engine  had abnormal flow gradient"<<endl;
             status=false;
         }
+
+        
+        return status;
     }
     else
     {
-        cout<<"Vacuum Engine  had abnormal flow gradient"<<endl;
-        status=false;
+        cout<<"Please switch Engine on"<<endl;
     }
-
-    retrieveMemento(storage->retrieve());     //restore the original fuel and temperature values 
-    return status;
 }
 
 VacuumEngine::~VacuumEngine()
