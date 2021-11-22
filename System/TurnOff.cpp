7b6ad3700c84e67135a53d28c6c905bbcaa4126e
Ponalo-ss <94800625+Ponalo-ss@users.noreply.github.com>
2021-11-22 20:00:18 +0200

    Update TurnOff.cpp

--- System/TurnOff.cpp
+++ System/TurnOff.cpp
@@ -1,21 +1,21 @@
 #include "TurnOff.h"
 
-TurnOff::TurnOff(Engine* e):Command(e)
+TurnOff::TurnOff(Engine* e):Command(e)    //The turnOff function that inherits from the command class
 {
 
 }
 
-void TurnOff::execute()
+void TurnOff::execute()      // The execute function that triggers the receiver's engine OFF function
 {
     getReceiver()->Off();
 }
 
-void TurnOff::undo()
+void TurnOff::undo()      //The undo function that triggers the receiver's ON function
 {
     getReceiver()->On();
 }
 
-TurnOff::~TurnOff()
+TurnOff::~TurnOff()       //The destructor
 {
 
 }
