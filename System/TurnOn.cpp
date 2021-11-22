8c13584281ce9b00a2b5a7d686d5532d0dcd47d0
Ponalo-ss <94800625+Ponalo-ss@users.noreply.github.com>
2021-11-22 19:51:58 +0200

    Update TurnOn.cpp

--- System/TurnOn.cpp
+++ System/TurnOn.cpp
@@ -1,19 +1,19 @@
 #include "TurnOn.h"
 
-TurnOn::TurnOn(Engine* e):Command(e)
+TurnOn::TurnOn(Engine* e):Command(e)  //The turn on fuction that inherits from the command class
 {
 }
 
-void TurnOn::execute()
+void TurnOn::execute()   // The execute function that triggers the receiver's engine ON function
 {
     getReceiver()->On();
 }
 
-void TurnOn::undo()
+void TurnOn::undo()   // The undo function that triggers the receiver's engine OFF function
 {
     getReceiver()->Off();
 }
 
-TurnOn::~TurnOn()
+TurnOn::~TurnOn()  // The destructor
 {
 }
